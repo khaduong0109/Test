239d631f130b4f11918e38c3d64b534dba49b220
Kha Duong <dbs.tekkin26@gmail.com>
2025-09-06 14:13:31 +0900

    check

--- project/梁配筋施工図.Canvas.cs
+++ project/梁配筋施工図.Canvas.cs
@@ -1289,7 +1289,7 @@ namespace RevitProjectDataAddin
                 allSolids.AddRange(ss);
             }
             WriteSimpleDxf(dlg.FileName, allLines, allTexts, allCircs, allSolids, flipY: true);
-            MessageBox.Show("DXF exported!fsdfgsdfsdfs");
+            MessageBox.Show("DXF exported!");
         }
 
         private void ExportItemDxf_Click(object sender, RoutedEventArgs e)
